ee9364a0c94af6a2eeed95d27ef7f1651add6e93
muhammad uzair khan <uzairkh52@gmail.com>
2025-02-24 07:24:37 +0500

    flight booking test v2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Avatar, Grid, Container, Menu, MenuItem, Box, Button } from "@mui/material";
-import "../styles/sass/style.scss";
+import "@/src/styles/sass/style.scss";
 import { Provider } from 'react-redux';
-import store from "../store/store"
+import store from "@/src/store/store"
 export default function App({ Component, pageProps }) {
   return (  
     <Provider store={store}>
