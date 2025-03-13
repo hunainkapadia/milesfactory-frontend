84e6c456231fd4064cfdbceb9c27c327aa3b454a
muhammad uzair khan <uzairkh52@gmail.com>
2025-03-13 12:15:07 +0500

    fav icon and chate link

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,13 +1,18 @@
 import HeroSection from "@/src/component/HeroSection";
 import styles from "@/src/styles/sass/components/Home.module.scss";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+   const sendMessages = useSelector((state) => state.sendMessage?.messages.length);
+   const getmessages = useSelector((state) => state.getMessages.messages.length); 
+   const isMessage = sendMessages > 0 || getmessages > 0; //check message length
+
    return (
       <main>
       <section
-        id="fold1"
-        className={styles.HomeBannerActive}
-      >
+          id="fold1"
+          className={`${!isMessage ? `${styles.HomeBanner} ` : ""}`}
+        >
         <HeroSection />
       </section>
     </main>
