35d36eb5851146218c5a5f789ead463954800417
muhammad uzair khan <uzairkh52@gmail.com>
2025-03-13 07:20:54 +0500

    mobile responsive

--- src/component/layout/Header/Navbar.js
+++ src/component/layout/Header/Navbar.js
@@ -1,26 +1,37 @@
 "use client";
-import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { Box, } from "@mui/material";
 import Link from "next/link";
 import styles from "@/src/styles/sass/components/baseLayout.module.scss"; // Import SCSS file
 
 const Navbar = () => {
   return (
-    <Box position="static" className={styles.navbar}
-      sx={{ display: { xs: "none", md: "block" } }} // Hide on mobile, show on desktop
-    >
+    <Box position="static" className={styles.navbar}>
       {/* Logo / Title */}
       {/* Navigation Links */}
-      <Box className={styles.navItems} display={"flex"} gap={3}>
+
+      <Box
+        className={styles.navItems}
+        display={"flex"}
+        gap={3}
+        Give
+        feedback
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", md: "row", lg: "row" }, // Row on mobile, Column on desktop
+          py: { xs: 3 },
+          gap: 3,
+          alignItems: { xs: "flex-start", lg: "center", md: "center" },
+        }}
+      >
         <Box component={Link} href="#" className={styles.navItem}>
-          Our vision
+          Our Vision
+        </Box>
+        <Box component={Link} href="#" className={styles.navItem}>
+          Give feedback
         </Box>
 
         <Box component={Link} href="#" className={styles.navItem}>
-        Give feedback
-        </Box>
-
-        <Box component={Link} href="#" className={styles.navItem}>
-        My trips
+          My trips
         </Box>
       </Box>
     </Box>
--- src/component/layout/Header/index.js
+++ src/component/layout/Header/index.js
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, Divider, Drawer, Grid, IconButton, Typography } from "@mui/material";
 import Link from "next/link";
 import styles from "@/src/styles/sass/components/baseLayout.module.scss";
 import Head from "next/head";
@@ -13,6 +13,8 @@ import { setLoginUser } from "@/src/store/slices/Auth/LoginSlice";
 const Header = () => {
   const [isSticky, setIsSticky] = useState(false);
   const dispatch = useDispatch()
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false); // State for drawer
+
   useEffect(() => {
     const handleScroll = () => {
       setIsSticky(window.scrollY > 50); // Add sticky class after scrolling 50px
@@ -45,6 +47,11 @@ const Header = () => {
     dispatch(logoutUser());
   };
 
+  const toggleDrawer = ()=> {
+    setIsDrawerOpen(!isDrawerOpen);
+
+  }
+
 
 
   return (
@@ -65,18 +72,85 @@ const Header = () => {
               justifyContent={"space-between"}
               alignItems={"center"}
             >
-              <Box width={"20%"} className={styles.Logo}>
-                <Link href={"/"}>
-                  <Box className="d-flex align-items-center">
-                    {isSticky || isMessage ? (
-                      <img src="/images/logo-color2.svg" />
-                    ) : (
-                      <img src="/images/mylz-logo-white.svg" />
-                    )}
-                  </Box>
-                </Link>
+              <Box
+                color="white"
+                className={styles.leftCol}
+                display={"flex"}
+                alignItems={"center"}
+                gap={2}
+              >
+                <Box fontSize={"24px"} >
+                <i onClick={toggleDrawer} class="fa fa-bars" aria-hidden="true"></i>
+                
+                </Box>
+
+                <Box className={styles.Logo}>
+                  <Link href={"/"}>
+                    <Box className="d-flex align-items-center">
+                      {isSticky || isMessage ? (
+                        <img src="/images/logo-color2.svg" />
+                      ) : (
+                        <img src="/images/mylz-logo-white.svg" />
+                      )}
+                    </Box>
+                  </Link>
+                </Box>
               </Box>
-              <Navbar />
+              <Drawer anchor="left" open={isDrawerOpen}  onClose={toggleDrawer}>
+                <Box
+                  className={styles.HeaderDrawer}
+                  sx={{
+                    px: { xs: 3 }, // Padding X (left & right) of 3 units only on extra-small (xs) screens
+                    py: 3
+                  }}
+                  width={"280px"}
+                >
+                  <Box
+                    display={"flex"}
+                    alignItems={"center"}
+                    gap={3}
+                  >
+                    {/* Close Button */}
+                    <Box fontSize={"24px"}>
+                    <i onClick={toggleDrawer} className="fa fa-arrow-left basecolor1"></i>
+
+                    </Box>
+                    <Box className={styles.Logo}>
+                      <Link href={"/"}>
+                        <Box className="d-flex align-items-center">
+                          <img src="/images/logo-color2.svg" />
+                        </Box>
+                      </Link>
+                    </Box>
+                  </Box>
+                  <Box>
+                    <Navbar />
+                    <Divider />
+                    <Typography
+                      variant="h5"
+                      pb={3}
+                      pt={5}
+                      textAlign={"center"}
+                      fontWeight={"medium"}
+                    >
+                      Fabrice El Gohary
+                    </Typography>
+                    <Box display={"flex"}>
+                      <Link
+                        href={""}
+                        className="w-100 btn btn-primary no-rounder btn-md"
+                      >
+                        Book a trip
+                      </Link>
+                    </Box>
+                  </Box>
+                </Box>
+
+                {/*  */}
+              </Drawer>
+              <Box sx={{ display: { xs: "none", md: "block" } }}>
+                <Navbar />
+              </Box>
 
               <Box display={"flex"} gap={4}>
                 {isUserLogin || isFormSupmit ? (
