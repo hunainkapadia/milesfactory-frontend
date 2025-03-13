96d658b3b467e6c14843f434af763a37623937b1
muhammad uzair khan <uzairkh52@gmail.com>
2025-03-13 12:21:09 +0500

    home page

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,4 +1,5 @@
 import HeroSection from "@/src/component/HeroSection";
+import Header from "@/src/component/layout/Header";
 import styles from "@/src/styles/sass/components/Home.module.scss";
 import { useSelector } from "react-redux";
 
@@ -11,9 +12,10 @@ const Home = () => {
       <main>
       <section
           id="fold1"
-          className={`${!isMessage ? `${styles.HomeBanner} ` : ""}`}
+          className={`${!isMessage ? `${styles.HomeBanner} ${styles.HomeBannerActive} ` : ""}`}
         >
         <HeroSection />
+         <Header />
       </section>
     </main>
    );
