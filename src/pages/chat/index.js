a63c7c5ca5883f33fcecb06df0031d999c9e8572
muhammad uzair khan <uzairkh52@gmail.com>
2025-03-16 01:16:20 +0500

    mesage change url

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,6 +1,8 @@
 import HeroSection from "@/src/component/HeroSection";
 import Header from "@/src/component/layout/Header";
 import styles from "@/src/styles/sass/components/Home.module.scss";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const Home = () => {
@@ -8,6 +10,12 @@ const Home = () => {
    const getmessages = useSelector((state) => state.getMessages.messages.length); 
    const isMessage = sendMessages > 0 || getmessages > 0; //check message length
 
+     const router = useRouter();
+     useEffect(() => {
+       if (!isMessage) {
+         router.replace("/")
+       }
+     }, [isMessage, router]);
    return (
       <main>
       <section
