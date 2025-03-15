90023321b7d9d559bd9ba9cd334acff461a5b705
muhammad uzair khan <uzairkh52@gmail.com>
2025-03-15 13:11:13 +0500

    style test v2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import store from "@/src/store/store"
+import React from "react";
+import { Provider } from "react-redux";
+import { StyledEngineProvider } from "@mui/material/styles"; // Ensures MUI styles don't override SCSS
+import store from "@/src/store/store";
 import Head from "next/head";
+
+// Always load global SCSS first
 import "@/src/styles/sass/style.scss";
+
 export default function App({ Component, pageProps }) {
-  return (  
+  return (
     <Provider store={store}>
-    <Head>
-        <link rel="icon" href="/images/favicon_mylz.svg" />
-      </Head>
-      <Component {...pageProps} />
-    </Provider>  
-    
-  )
+      {/* Ensure SCSS has higher priority over MUI styles */}
+      <StyledEngineProvider injectFirst={false}>
+        <Head>
+          <link rel="icon" href="/images/favicon_mylz.svg" />
+        </Head>
+        <Component {...pageProps} />
+      </StyledEngineProvider>
+    </Provider>
+  );
 }
