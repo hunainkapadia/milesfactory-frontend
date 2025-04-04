6842792e7573fa780d144451b95198eac4a88ab0
muhammad uzair khan <uzairkh52@gmail.com>
2025-04-04 17:18:03 +0500

    ui improvement

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,28 +1,38 @@
 import React from "react";
-import { Provider, useSelector } from "react-redux";
-import { StyledEngineProvider } from "@mui/material/styles";
+import { Provider } from "react-redux";
+import { StyledEngineProvider, ThemeProvider, createTheme } from "@mui/material/styles";
 import store from "@/src/store/store";
 import Head from "next/head";
+import CssBaseline from "@mui/material/CssBaseline";
 
 import "@/src/styles/sass/style.scss";
-// 👇 Create a wrapper to use Redux hooks safely
+
+// ✅ Define a Custom Theme with the New Font
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Open Sans', sans-serif", // Change to your preferred font
+  },
+});
+
 function AppWrapper({ Component, pageProps }) {
-  return (
-    <>
-       <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <StyledEngineProvider injectFirst={false}>
-        <Head>
-          <link rel="icon" href="/images/favicon_mylz_v2.svg" />
-        </Head>
-        {/* Now it's safe to use Redux hooks inside */}
-        <AppWrapper Component={Component} pageProps={pageProps} />
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline /> {/* Ensures global styles apply */}
+          <Head>
+            <link
+              href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
+              rel="stylesheet"
+            />
+            <link rel="icon" href="/images/favicon_mylz_v2.svg" />
+          </Head>
+          <AppWrapper Component={Component} pageProps={pageProps} />
+        </ThemeProvider>
       </StyledEngineProvider>
     </Provider>
   );
