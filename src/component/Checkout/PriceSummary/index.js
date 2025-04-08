645be65f6b3658b385b3cd1c7ba6690ed0ae5796
muhammad uzair khan <uzairkh52@gmail.com>
2025-04-08 05:02:32 +0500

    scroll isue fix and price sumry add price

--- src/component/Checkout/PriceSummary/index.js
+++ src/component/Checkout/PriceSummary/index.js
@@ -17,7 +17,7 @@ const PriceSummary = ({ getdata }) => {
 
   const dispatch = useDispatch();
   const handlePaymentDrawer = () => {
-    dispatch(setIsDrawer(true))
+    dispatch(setIsDrawer(true));
   };
 
   const priceSummaryHandle = () => {
@@ -32,12 +32,11 @@ const PriceSummary = ({ getdata }) => {
     }
   }, [priceSummary]);
 
-  // get flight 
+  // get flight
   const flightDetail = useSelector((state) => state.booking.flightDetail);
 
   console.log("flightDetailprice", flightDetail);
-  
-
+  const passengers = flightDetail.slices?.[0]?.segments?.[0]?.passengers || [];
 
   return (
     <>
@@ -59,52 +58,71 @@ const PriceSummary = ({ getdata }) => {
               Price summary
             </h4>
           </Box>
-          <Box className={styles.PriceSection + " basecolor"} pb={2}>
-            <Box
-              className={styles.PriceRow}
-              display={"flex"}
-              justifyContent={"space-between"}
-              gap={4}
-            >
-              <Box>LDN - BGK, Return / 2x adults</Box>
-              <Box>£1,304.70</Box>
+          <Box
+            className={styles.PriceSection + " basecolor"}
+            pb={2}
+            display={"flex"}
+            flexDirection={"column"}
+            gap={2}
+          >
+            <Box className={styles.BaggageBody}>
+              {/* Total price row */}
+              <Box
+                className={styles.PriceRow}
+                display="flex"
+                justifyContent="space-between"
+                gap={4}
+              >
+                <Box>
+                  {flightDetail.slices?.[0]?.origin.iata_code} -{" "}
+                  {flightDetail.slices?.at(0)?.destination.iata_code}, Return /{" "}
+                  {passengers.length}x{" "}
+                  {passengers.length > 1 ? "adults" : "adult"}
+                </Box>
+                <Box>
+                  {flightDetail.total_currency === "GBP"
+                    ? "£"
+                    : flightDetail.total_currency}
+                  {flightDetail.total_amount_plus_markup.toFixed(2)}
+                </Box>
+              </Box>
+
+              {/* Taxes, fees & surcharges row */}
+              <Box
+                className={styles.PriceRow}
+                display="flex"
+                justifyContent="space-between"
+                gap={4}
+              >
+                <Box>Taxes, fees & surcharges</Box>
+                <Box>
+                  {flightDetail.tax_currency === "GBP"
+                    ? "£"
+                    : flightDetail.tax_currency}
+                  {Number(flightDetail.tax_amount).toFixed(2)}
+                </Box>
+              </Box>
+              <Box
+                className={styles.PriceRow}
+                display="flex"
+                justifyContent="space-between"
+                gap={4}
+              >
+                <Box>2x upgraded seats</Box>
+                <Box>£46.00</Box>
+              </Box>
+              <Box
+                className={styles.PriceRow}
+                display="flex"
+                justifyContent="space-between"
+                gap={4}
+              >
+                <Box>2 x 15kg carry-on bags</Box>
+                <Box>£60.00</Box>
+              </Box>
+              
             </Box>
-            <Box
-              className={styles.PriceRow}
-              display={"flex"}
-              justifyContent={"space-between"}
-              gap={4}
-            >
-              <Box>LDN - BGK, Return / 2x adults</Box>
-              <Box>£1,304.70</Box>
-            </Box>
-            <Box
-              className={styles.PriceRow}
-              display={"flex"}
-              justifyContent={"space-between"}
-              gap={4}
-            >
-              <Box>LDN - BGK, Return / 2x adults</Box>
-              <Box>£1,304.70</Box>
-            </Box>
-            <Box
-              className={styles.PriceRow}
-              display={"flex"}
-              justifyContent={"space-between"}
-              gap={4}
-            >
-              <Box>LDN - BGK, Return / 2x adults</Box>
-              <Box>£1,304.70</Box>
-            </Box>
-            <Box
-              className={styles.PriceRow}
-              display={"flex"}
-              justifyContent={"space-between"}
-              gap={4}
-            >
-              <Box>LDN - BGK, Return / 2x adults</Box>
-              <Box>£1,304.70</Box>
-            </Box>
+            {/* price row */}
             <Box
               className={styles.PriceRow + " black exbold"}
               display={"flex"}
@@ -113,9 +131,9 @@ const PriceSummary = ({ getdata }) => {
             >
               <Box>Total price</Box>
               <h5 className="mb-0">
-              {currencySymbols[flightDetail?.tax_currency] ||
-                flightDetail?.tax_currency}{" "}
-                                {Math.round(flightDetail?.total_amount)}
+                {currencySymbols[flightDetail?.tax_currency] ||
+                  flightDetail?.tax_currency}{" "}
+                {Math.round(flightDetail?.total_amount)}
               </h5>
             </Box>
           </Box>
