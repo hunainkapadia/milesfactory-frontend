65e2853ea49aca088816981c3ea69022a35a933b
muhammad uzair khan <uzairkh52@gmail.com>
2025-04-20 09:05:40 +0500

    test deployment

--- src/component/SearchResult/PollingMessage/PollingMessage.js
+++ src/component/SearchResult/PollingMessage/PollingMessage.js
@@ -9,11 +9,11 @@ const PollingMessage = ({PollingData}) => {
   return (
     <Box className={searchResultStyles.AiMessage}>
       <Typography className="bold">
-        We have everything we need, now looking for flights:{" "}
+        We have everything we need, now looking for flights: <br />
         {PollingData?.adults} {PollingData?.adults === 1 ? "adult" : "adults"}
         {PollingData?.children > 0 && `, ${PollingData.children} child(ren)`}
         {PollingData?.infants > 0 && `, ${PollingData.infants} infant(s)`},{" "}
-        {PollingData?.cabin_class} class, {PollingData?.flight_type} trip, from{" "}
+        {PollingData?.cabin_class} class, {PollingData?.flight_type}, from{" "}
         {PollingData?.from_destination} to {PollingData?.to_destination},
         departing on{" "}
         {new Date(PollingData?.departure_date).toLocaleDateString("en-GB", {
