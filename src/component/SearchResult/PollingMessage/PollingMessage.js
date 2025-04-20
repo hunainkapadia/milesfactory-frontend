70e50f2f95911df2b433fb2dece9b7ae60f6c94e
muhammad uzair khan <uzairkh52@gmail.com>
2025-04-20 08:21:26 +0500

    polling arguments add

--- src/component/SearchResult/PollingMessage/PollingMessage.js
+++ src/component/SearchResult/PollingMessage/PollingMessage.js
@@ -9,29 +9,24 @@ const PollingMessage = ({PollingData}) => {
   return (
     <Box className={searchResultStyles.AiMessage}>
       <Typography className="bold">
-        {`We have everything we need, now looking for flights: 
-                ${PollingData?.adults} adult(s), 
-                ${PollingData?.children} child(ren), 
-                ${PollingData?.infants} infant(s), 
-                ${PollingData?.cabin_class} class, 
-                ${PollingData?.flight_type} trip, 
-                from ${PollingData?.from_destination} to ${
-          PollingData?.to_destination
-        }, 
-                departing on ${new Date(
-                  PollingData?.departure_date
-                ).toLocaleDateString("en-GB", {
-                  day: "2-digit",
-                  month: "short",
-                  year: "numeric",
-                })}, 
-                arriving on ${new Date(
-                  PollingData?.arrival_date
-                ).toLocaleDateString("en-GB", {
-                  day: "2-digit",
-                  month: "short",
-                  year: "numeric",
-                })}.`}
+        We have everything we need, now looking for flights:{" "}
+        {PollingData?.adults} {PollingData?.adults === 1 ? "adult" : "adults"}
+        {PollingData?.children > 0 && `, ${PollingData.children} child(ren)`}
+        {PollingData?.infants > 0 && `, ${PollingData.infants} infant(s)`},{" "}
+        {PollingData?.cabin_class} class, {PollingData?.flight_type} trip, from{" "}
+        {PollingData?.from_destination} to {PollingData?.to_destination},
+        departing on{" "}
+        {new Date(PollingData?.departure_date).toLocaleDateString("en-GB", {
+          day: "2-digit",
+          month: "short",
+          year: "numeric",
+        })}
+        , arriving on{" "}
+        {new Date(PollingData?.arrival_date).toLocaleDateString("en-GB", {
+          day: "2-digit",
+          month: "short",
+          year: "numeric",
+        })}
       </Typography>
     </Box>
   );
