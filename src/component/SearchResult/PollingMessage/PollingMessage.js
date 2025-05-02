41fe8aa2b876c15d89bdfd7d5c9f3cb9978be123
muhammad uzair khan <uzairkh52@gmail.com>
2025-05-02 14:22:13 +0500

    passenger data formating

--- src/component/SearchResult/PollingMessage/PollingMessage.js
+++ src/component/SearchResult/PollingMessage/PollingMessage.js
@@ -5,25 +5,47 @@ const PollingMessage = ({ PollingData }) => {
   console.log("PollingData", PollingData);
 
   return (
-    <Typography className="bold">
-      We have everything we need, now looking for flights: <br />
-      {PollingData?.adults} {PollingData?.adults === 1 ? "adult" : "adults"}
-      {PollingData?.children > 0 && `, ${PollingData.children} child(ren)`}
-      {PollingData?.infants > 0 && `, ${PollingData.infants} infant(s)`},{" "}
-      {PollingData?.cabin_class} class, {PollingData?.flight_type}, from{" "}
-      {PollingData?.from_destination} to {PollingData?.to_destination},
-      departing on{" "}
-      {new Date(PollingData?.departure_date).toLocaleDateString("en-GB", {
-        day: "2-digit",
-        month: "short",
-        year: "numeric",
-      })}
-      , returning on{" "}
-      {new Date(PollingData?.arrival_date).toLocaleDateString("en-GB", {
-        day: "2-digit",
-        month: "short",
-        year: "numeric",
-      })}
+    <Typography component="div">
+      We have everything we need. Searching flights with the following details:
+      <Box pt={2}>
+        <ul className="no-list">
+          <li style={{ marginBottom: "0.5rem" }}>
+            Passengers: {PollingData?.adults}{" "}
+            {PollingData?.adults === 1 ? "adult" : "adults"}
+            {PollingData?.children > 0 &&
+              `, ${PollingData.children} child(ren)`}
+            {PollingData?.infants > 0 && `, ${PollingData.infants} infant(s)`}
+          </li>
+          <li style={{ marginBottom: "0.5rem" }}>
+            Cabin: {PollingData?.cabin_class} class
+          </li>
+          <li style={{ marginBottom: "0.5rem" }}>
+            Trip type: {PollingData?.flight_type}
+          </li>
+          <li style={{ marginBottom: "0.5rem" }}>
+            From: {PollingData?.from_destination}
+          </li>
+          <li style={{ marginBottom: "0.5rem" }}>
+            To: {PollingData?.to_destination}
+          </li>
+          <li style={{ marginBottom: "0.5rem" }}>
+            Departure:{" "}
+            {new Date(PollingData?.departure_date).toLocaleDateString("en-GB", {
+              day: "2-digit",
+              month: "short",
+              year: "numeric",
+            })}
+          </li>
+          <li style={{ marginBottom: "0.5rem" }}>
+            Return:{" "}
+            {new Date(PollingData?.arrival_date).toLocaleDateString("en-GB", {
+              day: "2-digit",
+              month: "short",
+              year: "numeric",
+            })}
+          </li>
+        </ul>
+      </Box>
     </Typography>
   );
 };
