469b44cd41c036ac56891c3a022a752327bc0f59
muhammad uzair khan <uzairkh52@gmail.com>
2025-06-02 14:35:40 +0500

    profile and token issue fix

--- src/store/tokenHelpers.js
+++ src/store/tokenHelpers.js
@@ -1,17 +1,16 @@
-import { jwtDecode } from "jwt-decode";
+// utils/tokenHelpers.js
 
 export function isTokenExpired(token) {
   try {
-    const decoded = jwtDecode(token);
+    const [, payload] = token.split(".");
+    const decoded = JSON.parse(atob(payload));
     const currentTime = Math.floor(Date.now() / 1000);
 
-    console.log("Decoded token:", decoded);
-    console.log("Current time:", currentTime);
-    console.log("Token exp:", decoded.exp);
+    
 
     return decoded.exp < currentTime;
   } catch (e) {
     console.error("❌ Failed to decode token", e);
-    return true;
+    return true; // consider expired if decoding fails
   }
 }
