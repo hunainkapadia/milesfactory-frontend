a5b88654135059ebaa5b2becd37954cb3c16b57c
muhammad uzair khan <uzairkh52@gmail.com>
2025-06-06 14:50:29 +0500

    implement google anylyticts code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Provider } from "react-redux";
-import { StyledEngineProvider, ThemeProvider, createTheme } from "@mui/material/styles";
+import {
+  StyledEngineProvider,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material/styles";
 import store from "@/src/store/store";
 import Head from "next/head";
 import CssBaseline from "@mui/material/CssBaseline";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import "@/src/styles/sass/style.scss";
-import 'react-phone-input-2/lib/style.css';
+import "react-phone-input-2/lib/style.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { useRouter } from "next/router";
 
-// ✅ Define a Custom Theme with the New Font
+// Define a Custom Theme with the New Font
 const theme = createTheme({
   typography: {
     fontFamily: "'Open Sans', sans-serif", // Change to your preferred font
@@ -22,6 +27,20 @@ function AppWrapper({ Component, pageProps }) {
 }
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag?.("config", "G-0MNTS4RLHH", {
+        page_path: url,
+      });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <Provider store={store}>
       <GoogleOAuthProvider clientId="830627155853-a0kn9u3fcttld40vhkr90olun09dhvmp.apps.googleusercontent.com">
@@ -36,7 +55,10 @@ export default function App({ Component, pageProps }) {
               <link rel="icon" href="/images/favicon_mylz_v2.svg" />
 
               {/* Google Analytics */}
-              <script async src="https://www.googletagmanager.com/gtag/js?id=G-0MNTS4RLHH"></script>
+              <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=G-0MNTS4RLHH"
+              ></script>
               <script
                 dangerouslySetInnerHTML={{
                   __html: `
@@ -48,7 +70,6 @@ export default function App({ Component, pageProps }) {
                 }}
               />
               {/* Google Analytics */}
-              
             </Head>
             <AppWrapper Component={Component} pageProps={pageProps} />
           </ThemeProvider>
