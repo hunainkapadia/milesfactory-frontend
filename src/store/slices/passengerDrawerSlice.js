1377415a69cf86910960598a9d49df4777c5acaf
muhammad uzair khan <uzairkh52@gmail.com>
2025-06-06 06:39:39 +0500

    profile drawer issue fix

--- src/store/slices/passengerDrawerSlice.js
+++ src/store/slices/passengerDrawerSlice.js
@@ -270,7 +270,6 @@ export const PassengerFormSubmit = (params) => (dispatch, getState) => {
         dispatch(ViewPassengers());
       }, 500);
       dispatch(setClosePassengerDrawer());
-      dispatch(setPassProfileDrawer(false)); //close profile drawer
       
     })
     .catch((error) => {
