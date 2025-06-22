afac77a324484401551b5d62962478b00a9539dd
muhammad uzair khan <uzairkh52@gmail.com>
2025-06-22 16:53:48 +0500

    search new design

--- src/component/SearchResult/SearchFilterBar/index.js
+++ src/component/SearchResult/SearchFilterBar/index.js
@@ -14,102 +14,106 @@ const SearchFilterBar = () => {
   console.log("SearchHistory2", SearchHistory?.children);
   return (
     <>
-      <Box
-        component={"main"}
-        className={styles.SearchFilterBar}
-        sx={{ display: { xs: "block", lg: "none", md: "none" } }}
-      >
-        <Box component={"section"} className={styles.Content}>
-          {/*  */}
-          <Box gap={2} display={"flex"} alignItems={"center"}>
-            <Box className=" imggroup" sx={{ width: "28px" }}>
-              <img width="28" src="/images/plane-icon-basecolor1.svg" />
+    {SearchHistory && (
+      <>
+         <Box
+         component={"main"}
+         className={styles.SearchFilterBar}
+         sx={{ display: { xs: "block", lg: "none", md: "none" } }}
+         >
+         <Box component={"section"} className={styles.Content}>
+            {/*  */}
+            <Box gap={2} display={"flex"} alignItems={"center"}>
+               <Box className=" imggroup" sx={{ width: "28px" }}>
+               <img width="28" src="/images/plane-icon-basecolor1.svg" />
+               </Box>
+               {SearchHistory ? (
+               <Box className={styles.Header2 + " aaa"}>
+                  <Box mb={"2px"}>
+                     <Typography className="bold f10">
+                     {SearchHistory?.from_title} - {SearchHistory?.to_title}
+                     </Typography>
+                  </Box>
+                  <Box
+                     mb={"3px"}
+                     className={styles.filterRrow}
+                     display={"flex"}
+                     alignItems={"center"}
+                  >
+                     <Typography className=" f12 black regular">
+                     {SearchHistory?.departure_date ? (
+                        <>
+                           {new Date(
+                           SearchHistory?.departure_date
+                           ).toLocaleDateString("en-GB", {
+                           day: "2-digit",
+                           month: "short",
+                           })}
+                        </>
+                     ) : (
+                        ""
+                     )}
+                     {SearchHistory?.arrival_date ? (
+                        <>
+                           {" - "}
+                           {new Date(
+                           SearchHistory.arrival_date
+                           ).toLocaleDateString("en-GB", {
+                           day: "2-digit",
+                           month: "short",
+                           })}
+                        </>
+                     ) : (
+                        ""
+                     )}
+                     </Typography>
+                     <Typography className=" f12 black regular">
+                     {SearchHistory.flight_type == "round-trip"
+                        ? "Return"
+                        : "One way"}
+                     </Typography>
+                     <Typography className="f12 black regular">
+                     {[
+                        SearchHistory?.adults > 0 &&
+                           `${SearchHistory.adults} ${
+                           SearchHistory.adults === 1 ? "adult" : "adults"
+                           }`,
+                        SearchHistory?.children > 0 &&
+                           `${SearchHistory.children} ${
+                           SearchHistory.children === 1 ? "child" : "children"
+                           }`,
+                        SearchHistory?.infants > 0 &&
+                           `${SearchHistory.infants} ${
+                           SearchHistory.infants === 1 ? "infant" : "infants"
+                           }`,
+                     ]
+                        .filter(Boolean)
+                        .join(", ")}
+                     </Typography>
+                  </Box>
+                  <Box
+                     className={styles.filterRrow2}
+                     display={"flex"}
+                     alignItems={"center"}
+                  >
+                     <Typography fontSize={8} className=" gray regular">
+                     Filters: none
+                     </Typography>
+                     <Typography fontSize={8} className=" gray regular">
+                     Sorting: best match
+                     </Typography>
+                  </Box>
+                  {/*  */}
+               </Box>
+               ) : (
+               ""
+               )}
             </Box>
-            {SearchHistory ? (
-              <Box className={styles.Header2 + " aaa"}>
-                <Box mb={"2px"}>
-                  <Typography className="bold f10">
-                    {SearchHistory?.from_title} - {SearchHistory?.to_title}
-                  </Typography>
-                </Box>
-                <Box
-                  mb={"3px"}
-                  className={styles.filterRrow}
-                  display={"flex"}
-                  alignItems={"center"}
-                >
-                  <Typography className=" f12 black regular">
-                    {SearchHistory?.departure_date ? (
-                      <>
-                        {new Date(
-                          SearchHistory?.departure_date
-                        ).toLocaleDateString("en-GB", {
-                          day: "2-digit",
-                          month: "short",
-                        })}
-                      </>
-                    ) : (
-                      ""
-                    )}
-                    {SearchHistory?.arrival_date ? (
-                      <>
-                        {" - "}
-                        {new Date(
-                          SearchHistory.arrival_date
-                        ).toLocaleDateString("en-GB", {
-                          day: "2-digit",
-                          month: "short",
-                        })}
-                      </>
-                    ) : (
-                      ""
-                    )}
-                  </Typography>
-                  <Typography className=" f12 black regular">
-                    {SearchHistory.flight_type == "round-trip"
-                      ? "Return"
-                      : "One way"}
-                  </Typography>
-                  <Typography className="f12 black regular">
-                    {[
-                      SearchHistory?.adults > 0 &&
-                        `${SearchHistory.adults} ${
-                          SearchHistory.adults === 1 ? "adult" : "adults"
-                        }`,
-                      SearchHistory?.children > 0 &&
-                        `${SearchHistory.children} ${
-                          SearchHistory.children === 1 ? "child" : "children"
-                        }`,
-                      SearchHistory?.infants > 0 &&
-                        `${SearchHistory.infants} ${
-                          SearchHistory.infants === 1 ? "infant" : "infants"
-                        }`,
-                    ]
-                      .filter(Boolean)
-                      .join(", ")}
-                  </Typography>
-                </Box>
-                <Box
-                  className={styles.filterRrow2}
-                  display={"flex"}
-                  alignItems={"center"}
-                >
-                  <Typography fontSize={8} className=" gray regular">
-                    Filters: none
-                  </Typography>
-                  <Typography fontSize={8} className=" gray regular">
-                    Sorting: best match
-                  </Typography>
-                </Box>
-                {/*  */}
-              </Box>
-            ) : (
-              ""
-            )}
-          </Box>
-        </Box>
-        {/*  */}
-      </Box>
+         </Box>
+         {/*  */}
+         </Box>
+      </>
+    )}
     </>
   );
 };
