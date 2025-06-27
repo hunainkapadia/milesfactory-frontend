e0a927e40a3e291172c8676e8c5b36969d5d5322
Hunain Kapadia <hunain88@gmail.com>
2025-06-27 23:36:51 +0500

    remove summary in chat

--- src/component/SearchResult/PollingMessage/PollingMessage.js
+++ src/component/SearchResult/PollingMessage/PollingMessage.js
@@ -23,64 +23,7 @@ const PollingMessage = ({ PollingData }) => {
   return (
     <>
       <Typography component="div">
-        We have everything we need. Searching flights with the following
-        details:
-        <Box pt={2}>
-          <ul className="no-list">
-            <li style={{ marginBottom: "0.5rem" }}>
-              Passengers: {SearchHistory?.adults}{" "} 
-              {SearchHistory?.adults === 1 ? `adult` : `adults`}
-              {SearchHistory?.children?.length > 0 &&
-                `, ${SearchHistory.children.length} ${
-                  SearchHistory.children.length === 1 ? "child" : "children"
-                }`}
-              {SearchHistory?.infants > 0 &&
-                `, ${SearchHistory.infants} ${
-                  SearchHistory.infants === 1 ? "infant" : "infants"
-                }`}
-            </li>
-
-            <li style={{ marginBottom: "0.5rem" }}>
-              Cabin: {SearchHistory?.cabin_class}
-            </li>
-            <li style={{ marginBottom: "0.5rem" }}>
-              Trip type: {SearchHistory?.flight_type}
-            </li>
-            <li style={{ marginBottom: "0.5rem" }}>
-              From: {SearchHistory?.from_destination}
-            </li>
-            <li style={{ marginBottom: "0.5rem" }}>
-              To: {SearchHistory?.to_destination}
-            </li>
-            <li style={{ marginBottom: "0.5rem" }}>
-              Departure:{" "}
-              {new Date(SearchHistory?.departure_date).toLocaleDateString(
-                "en-GB",
-                {
-                  day: "2-digit",
-                  month: "short",
-                  year: "numeric",
-                }
-              )}
-            </li>
-            {console.log("SearchHistory_arrival_date", SearchHistory)}
-            {SearchHistory?.flight_type !== "one-way" ? (
-              <li style={{ marginBottom: "0.5rem" }}>
-                Arrival:{" "}
-                {new Date(SearchHistory?.arrival_date).toLocaleDateString(
-                  "en-GB",
-                  {
-                    day: "2-digit",
-                    month: "short",
-                    year: "numeric",
-                  }
-                )}
-              </li>
-            ) : (
-              ""
-            )}
-          </ul>
-        </Box>
+        Looking for flights. Please wait...
       </Typography>
       {!pollingComplete ? (
         <Box mt={3}>
