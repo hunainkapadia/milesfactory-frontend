dcb722aaf6317693a413ac1d8612af558f8a35a4
muhammad uzair khan <uzairkh52@gmail.com>
2025-07-27 09:59:09 +0500

    comments linnk copied for chat

--- src/component/layout/Header/index.js
+++ src/component/layout/Header/index.js
@@ -172,7 +172,7 @@ const Header = ({ isMessage, IsActive, isHome, isChat, isUser }) => {
                     </Box>
                   </Box>
                 </Box>
-                <ShareDropdown />
+                {isMessage ? <ShareDropdown /> : ""}
 
                 <Box
                   sx={{
