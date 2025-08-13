eaeaf83955cfadd3f55ef7139b025175f5d75b74
muhammad uzair khan <uzairkh52@gmail.com>
2025-08-13 16:57:10 +0500

    my trip header formating issue fix

--- src/pages/my-trips/index.js
+++ src/pages/my-trips/index.js
@@ -18,6 +18,7 @@ import { setisUserPopup } from "@/src/store/slices/Auth/SignupSlice";
 import {
   createThread,
   createThreadAndRedirect,
+  deleteAndCreateThread,
 } from "@/src/store/slices/sendMessageSlice";
 import { useRouter } from "next/router";
 
@@ -49,8 +50,10 @@ const MyTrips = () => {
   };
 
   const HandleBookThread = () => {
-    dispatch(createThreadAndRedirect(router));
-  };
+      dispatch(deleteAndCreateThread({ isMessage: "forBook" }));
+  
+    };
+  
 
   return (
     <>
@@ -80,7 +83,7 @@ const MyTrips = () => {
               </Box>
 
               <Button
-                onClick={currentUser ? HandleBookThread : HandlePopup}
+                onClick={HandleBookThread}
                 sx={{ display: { lg: "block", md: "block", xs: "none" } }}
                 className="btn btn-primary btn-round btn-sm"
               >
