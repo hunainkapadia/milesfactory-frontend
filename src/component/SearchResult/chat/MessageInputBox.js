9d538d9e2b09a1f624235555accd13d6a1291ca8
muhammad uzair khan <uzairkh52@gmail.com>
2025-09-09 18:40:17 +0500

    tooltip and merge form to demo

--- src/component/SearchResult/chat/MessageInputBox.js
+++ src/component/SearchResult/chat/MessageInputBox.js
@@ -8,6 +8,7 @@ import {
   useMediaQuery,
   CircularProgress,
   Stack,
+  Tooltip,
 } from "@mui/material";
 import styles from "@/src/styles/sass/components/Home.module.scss";
 import MicAnimation from "../ChatInput/MicAnimation";
@@ -268,21 +269,26 @@ const MessageInputBox = ({
                       >
                         {isChat && isMobile ? (
                           <>
-                          <Box className={
-                                inputStyles.leftCol +
-                                ""
-                              }>
-                            <Box
-                              onClick={HandleNewThread}
-                              className={
-                                styles.newChatBtn +
-                                " newChatBtn lg cursor-pointer"
-                              }
-                            >
-                              <FontAwesomeIcon className="basecolor" icon={faPlus} />
+                            <Box className={inputStyles.leftCol + ""}>
+                              <Box
+                                onClick={HandleNewThread}
+                                className={
+                                  styles.newChatBtn +
+                                  " newChatBtn lg cursor-pointer"
+                                }
+                              >
+                                <FontAwesomeIcon
+                                  className="basecolor"
+                                  icon={faPlus}
+                                />
+                              </Box>
                             </Box>
-                          </Box>
-                            <Box display={"flex"} justifyContent={"center"} className={inputStyles.centerCol} sx={{ opacity: !getBuilder ? 0.5 : 1 }}>
+                            <Box
+                              display={"flex"}
+                              justifyContent={"center"}
+                              className={inputStyles.centerCol}
+                              sx={{ opacity: !getBuilder ? 0.5 : 1 }}
+                            >
                               <MobileBuilder />
                             </Box>
                           </>
@@ -302,29 +308,63 @@ const MessageInputBox = ({
                                   " newChatBtn dark cursor-pointer"
                                 }
                               >
-                                <FontAwesomeIcon className="white" icon={faPlus} />
+                                <FontAwesomeIcon
+                                  className="white"
+                                  icon={faPlus}
+                                />
                               </Box>
-                              <Box
-                                onClick={HandleNewThread}
-                                className={`${styles.newChatBtn} ${styles.publicBtn} newChatBtn auto cursor-pointer`}
-                                whiteSpace={"nowrap"}
-                                gap={"6px"}
-                                px={"12px"}
+
+                              <Tooltip
+                                title={
+                                  <>
+                                    <Typography variant="body2">
+                                      1. Share documents
+                                    </Typography>
+                                    <Typography variant="body2">
+                                      2. Change the privacy of your trip
+                                    </Typography>
+                                    <Typography variant="body2">
+                                      3. Record a message
+                                    </Typography>
+                                    <Typography variant="body2">
+                                      4. Start your trip now!
+                                    </Typography>
+                                  </>
+                                }
+                                arrow
+                                placement="bottom"
+                                componentsProps={{
+    tooltip: {
+      sx: {
+        bgcolor: "#222", // Change background color here
+        color: "#fff",       // Tooltip text color
+        fontSize: "14px",
+        padding: "8px",
+      },
+    },
+  }}
                               >
-                                <svg
-                                  width="10"
-                                  height="11"
-                                  viewBox="0 0 10 11"
-                                  fill="none"
-                                  xmlns="http://www.w3.org/2000/svg"
+                                <Box
+                                  className={`${styles.newChatBtn} ${styles.publicBtn} newChatBtn auto cursor-pointer`}
+                                  whiteSpace={"nowrap"}
+                                  gap={"6px"}
+                                  px={"12px"}
                                 >
-                                  <path
-                                    d="M5 10.5C4.30833 10.5 3.65833 10.3688 3.05 10.1062C2.44167 9.84375 1.9125 9.4875 1.4625 9.0375C1.0125 8.5875 0.65625 8.05833 0.39375 7.45C0.13125 6.84167 0 6.19167 0 5.5C0 4.80833 0.13125 4.15833 0.39375 3.55C0.65625 2.94167 1.0125 2.4125 1.4625 1.9625C1.9125 1.5125 2.44167 1.15625 3.05 0.89375C3.65833 0.63125 4.30833 0.5 5 0.5C5.69167 0.5 6.34167 0.63125 6.95 0.89375C7.55833 1.15625 8.0875 1.5125 8.5375 1.9625C8.9875 2.4125 9.34375 2.94167 9.60625 3.55C9.86875 4.15833 10 4.80833 10 5.5C10 6.19167 9.86875 6.84167 9.60625 7.45C9.34375 8.05833 8.9875 8.5875 8.5375 9.0375C8.0875 9.4875 7.55833 9.84375 6.95 10.1062C6.34167 10.3688 5.69167 10.5 5 10.5ZM5 9.5C6.11667 9.5 7.0625 9.1125 7.8375 8.3375C8.6125 7.5625 9 6.61667 9 5.5C9 5.44167 8.99792 5.38125 8.99375 5.31875C8.98958 5.25625 8.9875 5.20417 8.9875 5.1625C8.94583 5.40417 8.83333 5.60417 8.65 5.7625C8.46667 5.92083 8.25 6 8 6H7C6.725 6 6.48958 5.90208 6.29375 5.70625C6.09792 5.51042 6 5.275 6 5V4.5H4V3.5C4 3.225 4.09792 2.98958 4.29375 2.79375C4.48958 2.59792 4.725 2.5 5 2.5H5.5C5.5 2.30833 5.55208 2.13958 5.65625 1.99375C5.76042 1.84792 5.8875 1.72917 6.0375 1.6375C5.87083 1.59583 5.70208 1.5625 5.53125 1.5375C5.36042 1.5125 5.18333 1.5 5 1.5C3.88333 1.5 2.9375 1.8875 2.1625 2.6625C1.3875 3.4375 1 4.38333 1 5.5H3.5C4.05 5.5 4.52083 5.69583 4.9125 6.0875C5.30417 6.47917 5.5 6.95 5.5 7.5V8H4V9.375C4.16667 9.41667 4.33125 9.44792 4.49375 9.46875C4.65625 9.48958 4.825 9.5 5 9.5Z"
-                                    fill="white"
-                                  />
-                                </svg>
-                                <Typography>Public</Typography>
-                              </Box>
+                                  <svg
+                                    width="10"
+                                    height="11"
+                                    viewBox="0 0 10 11"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                  >
+                                    <path
+                                      d="M5 10.5C4.30833 10.5 3.65833 10.3688 3.05 10.1062C2.44167 9.84375 1.9125 9.4875 1.4625 9.0375C1.0125 8.5875 0.65625 8.05833 0.39375 7.45C0.13125 6.84167 0 6.19167 0 5.5C0 4.80833 0.13125 4.15833 0.39375 3.55C0.65625 2.94167 1.0125 2.4125 1.4625 1.9625C1.9125 1.5125 2.44167 1.15625 3.05 0.89375C3.65833 0.63125 4.30833 0.5 5 0.5C5.69167 0.5 6.34167 0.63125 6.95 0.89375C7.55833 1.15625 8.0875 1.5125 8.5375 1.9625C8.9875 2.4125 9.34375 2.94167 9.60625 3.55C9.86875 4.15833 10 4.80833 10 5.5C10 6.19167 9.86875 6.84167 9.60625 7.45C9.34375 8.05833 8.9875 8.5875 8.5375 9.0375C8.0875 9.4875 7.55833 9.84375 6.95 10.1062C6.34167 10.3688 5.69167 10.5 5 10.5ZM5 9.5C6.11667 9.5 7.0625 9.1125 7.8375 8.3375C8.6125 7.5625 9 6.61667 9 5.5C9 5.44167 8.99792 5.38125 8.99375 5.31875C8.98958 5.25625 8.9875 5.20417 8.9875 5.1625C8.94583 5.40417 8.83333 5.60417 8.65 5.7625C8.46667 5.92083 8.25 6 8 6H7C6.725 6 6.48958 5.90208 6.29375 5.70625C6.09792 5.51042 6 5.275 6 5V4.5H4V3.5C4 3.225 4.09792 2.98958 4.29375 2.79375C4.48958 2.59792 4.725 2.5 5 2.5H5.5C5.5 2.30833 5.55208 2.13958 5.65625 1.99375C5.76042 1.84792 5.8875 1.72917 6.0375 1.6375C5.87083 1.59583 5.70208 1.5625 5.53125 1.5375C5.36042 1.5125 5.18333 1.5 5 1.5C3.88333 1.5 2.9375 1.8875 2.1625 2.6625C1.3875 3.4375 1 4.38333 1 5.5H3.5C4.05 5.5 4.52083 5.69583 4.9125 6.0875C5.30417 6.47917 5.5 6.95 5.5 7.5V8H4V9.375C4.16667 9.41667 4.33125 9.44792 4.49375 9.46875C4.65625 9.48958 4.825 9.5 5 9.5Z"
+                                      fill="white"
+                                    />
+                                  </svg>
+                                  <Typography>Public</Typography>
+                                </Box>
+                              </Tooltip>
                             </Stack>
                           )}
                           <Box width={"100%"}>
