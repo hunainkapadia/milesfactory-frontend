ae67ca3f480579d260f2905c3680c1716be3812d
muhammad uzair khan <uzairkh52@gmail.com>
2025-10-28 17:49:17 +0500

    home heading change

--- src/component/home/HerosectionContent/index.js
+++ src/component/home/HerosectionContent/index.js
@@ -12,17 +12,35 @@ const HerosectionContent = () => {
            flexDirection={"column"}
          >
            <Box className="1111" sx={{ px: { xs: 0, lg: 2 } }}>
-             <Typography
-               component={"h1"}
-               sx={{
-                 lineHeight: "120%",
-                 mb: "6px",
-                 fontSize: { lg: "48px", md: "48px", xs: "36px" },
-               }}
-               className=" bold test-5.1"
+             <Box
+               display={"flex"}
+               alignItems={"center"}
+               justifyContent={"center"}
+               gap={1}
+               mb={1}
              >
-               Travel, unified
-             </Typography>
+               <Typography
+                 component={"h1"}
+                 mb={0}
+                 sx={{
+                   lineHeight: "120%",
+                   fontSize: { lg: "48px", md: "48px", xs: "36px" },
+                 }}
+                 className=" exbold test-5.1"
+               >
+                 Travel, crafted
+               </Typography>
+
+               <Box
+                 className="imggroup"
+                 sx={{ display: { md: "block", xs: "none" } }}
+               >
+                 <img
+                   alt="Travel, crafted"
+                   src="/images/dallar-craft-icon.svg"
+                 />
+               </Box>
+             </Box>
              <Typography
                sx={{ display: { lg: "block", md: "block", xs: "none" } }}
                color="white"
